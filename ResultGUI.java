ca8347aaf06319d7f82a41fd6b1e31cf81a4f31c
Charainia <charain.fong@ucdconnect.ie>
2016-05-01 14:59:14 +0100

    Added files via upload
    Need changing on only 4 lines of codes in startGUI.java

---
+++ ResultGUI.java
@@ -0,0 +1,136 @@
+package groupProject;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+import java.util.Vector;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.border.BevelBorder;
+import javax.swing.table.DefaultTableModel;
+
+public class ResultGUI 
+{
+	
+	JFrame frame;
+	private JTable tbpat;
+	String head2[] ={ "Name","Project Title"};	//define table's header  
+	DefaultTableModel dtm;	//initialize table controller variable 
+	JScrollPane jsp;	//initialize scroll bar controller variable
+	ResultSet rs1, rs2;
+	CandidateSolution table;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) 
+	{
+   		EventQueue.invokeLater(new Runnable() 
+   		{
+			public void run() 
+			{
+				try 
+				{
+					DisplayCand c=new DisplayCand(null); 
+					c.frame.setVisible(true);
+				} catch (Exception e) 
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	
+
+	public void showRecords()
+	{
+		dtm.setRowCount(0);
+		System.gc();
+		try
+		{
+			int len=table.getAssignments().size()-2;
+			Vector<CandidateAssignment> a=table.getAssignments();
+			while (len>=0)	//iterate until last row 
+			{
+				String temp;
+				String array[] = new String[2];
+				temp=a.get(len).toString();
+				String[] temp1=temp.split("Project:");
+				array[0]=temp1[0].replace("Name:", "").replace(" Name", "");
+				array[1]=temp1[1];
+				//System.out.println(len+" "+array[0]+" p= "+array[1]);
+				
+                len--;
+                
+				dtm.addRow(array);	//add row into table
+			}
+
+			}
+		catch (Exception e)
+		{
+			System.out.println("Error :" + e); 
+			e.printStackTrace();
+		}
+		JOptionPane.showMessageDialog(null, "Happiness= "+ table.getEnergy() +"%");
+	}
+
+	
+	/**
+	 * @wbp.parser.entryPoint
+	 */
+	/*Creates table with result from solution1.*/
+	public ResultGUI(CandidateSolution solution1) 
+	{
+		initialize();
+		table = solution1;
+		dtm = new DefaultTableModel();
+		dtm.setColumnIdentifiers(head2);	//set column header
+		tbpat = new JTable(dtm);
+		jsp = new JScrollPane(tbpat);
+		jsp.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		jsp.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+		jsp.setBounds(21, 26, 515, 334);
+		frame.getContentPane().add(jsp);	//add scrollbar 
+		tbpat.setRowHeight(20);
+		
+		JButton btnNewButton = new JButton("Cancel");
+		btnNewButton.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				System.exit(0);
+			}
+		});
+		btnNewButton.setBounds(232, 385, 89, 23);
+		frame.getContentPane().add(btnNewButton);
+		
+		tbpat = new JTable();
+		tbpat.setBounds(21, 26, 473, 323);
+		tbpat.setBorder(new BevelBorder(BevelBorder.LOWERED, new Color(0, 0, 0), new Color(0, 0, 0), new Color(0, 0, 0), new Color(0, 0, 0)));
+		tbpat.setBackground(new Color(192, 192, 192));
+		frame.getContentPane().add(tbpat);
+		showRecords();	//call showRecords() method 
+		
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() 
+	{
+		frame = new JFrame();
+		frame.setBounds(150, 130, 579, 453);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+	}
+}
+
+
+
---
+++ StartGUI.java
@@ -0,0 +1,125 @@
+package groupProject;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.BevelBorder;
+
+public class StartGUI 
+{
+	JFrame frame;
+	CandidateSolution cand;
+	JFrame jf;
+
+	/**
+	 * Creates the application.
+	 * @param cand 
+	 */
+	public StartGUI(CandidateSolution cand) 
+	{
+		this.cand=cand;
+		initialize();
+	}
+	public StartGUI() 
+	{
+		initialize();
+	}
+	/**
+	 * Initializes the contents of the frame.
+	 */
+	private void initialize() 
+	{
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		/*Lets the user chooses which method they want to use.*/
+		JButton btnSimulatin = new JButton("Simulated Annealing");
+		
+		/*If the Simulated Annealing button is pressed, asks the user if they want to change the cooling schedule.*/
+		btnSimulatin.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
+                int DResult = JOptionPane.showConfirmDialog(null,"Do you wish to change the cooling schedule?");
+                
+                /*If the user doesn't want to change the schedule, the program runs with the default schedule.*/
+                if (DResult == JOptionPane.NO_OPTION) 
+                {
+                   // JOptionPane.showMessageDialog(null,"Deletion Cancel", "DELETION",JOptionPane.DEFAULT_OPTION);
+    				SimulatedAnnealing s = new SimulatedAnnealing();
+    				CandidateSolution solution1 = s.getBestSolution(cand, 1000, 1);
+                	ResultGUI outcome = new ResultGUI(solution1);
+                	outcome.frame.setVisible(true);   	
+                }
+                
+                /*If the yes button is pressed, asks the user what temperature they want to change to.*/
+                if (DResult == JOptionPane.YES_OPTION)
+			    {
+                    String temperature = JOptionPane.showInputDialog("Enter temperature:");
+                    SimulatedAnnealing s=new SimulatedAnnealing();
+                	//s.change(cand, temperature);
+                    CandidateSolution solution1=s.getBestSolution(cand, Integer.parseInt(temperature), 1);
+                	ResultGUI outcome = new ResultGUI(solution1);
+                	outcome.frame.setVisible(true);
+			    }			
+			}
+		});
+		btnSimulatin.setBounds(60, 184, 178, 29);
+		frame.getContentPane().add(btnSimulatin);
+		
+		/*If the user chooses the Genetic Algorithm method.*/
+		JButton btnNewButton = new JButton("Genetic Algorithm");
+		btnNewButton.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
+                int DResult = JOptionPane.showConfirmDialog(null,"Would you like to change the number of generations?");
+                
+                /*If the user doesn't wanna change the number of generations, the program runs with the default number.*/
+                if (DResult == JOptionPane.NO_OPTION) 
+                {
+                   // JOptionPane.showMessageDialog(null,"Deletion Cancel", "DELETION",JOptionPane.DEFAULT_OPTION);
+    				GeneticAlgorithm g = new GeneticAlgorithm(); //NEEDS CHANGING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! WHATEVER NAME IS IT FOR GA!!!!!!!!!!!!!!!!!!!!
+    				CandidateSolution solution1 = g.getBestSolution(cand, 1000, 1); // getBestSolution(cand, 1000, 1) NEEDS CHANGING!!!! DONT SEE CODE FOR GA!!! 
+                	ResultGUI outcome=new ResultGUI(solution1);
+                	outcome.frame.setVisible(true);   	
+                }
+                
+                /*If the yes button is pressed, asks the user what amount of generations they want to change to.*/
+                if (DResult == JOptionPane.YES_OPTION)
+			    {
+                    String generations = JOptionPane.showInputDialog("Enter the amount of times you want the algorithm to run:");
+    				GeneticAlgorithm g = new GeneticAlgorithm(); //NEEDS CHANGING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! WHATEVER NAME IS IT FOR GA!!!!!!!!!!!!!!!!!!!!
+                	//g.change(cand, generations); NEEDS CHANGING TOO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    				CandidateSolution solution1 = g.getBestSolution(cand, 1000, 1); // getBestSolution(cand, 1000, 1) NEEDS CHANGING!!!! DONT SEE CODE FOR GA!!! 
+                	ResultGUI outcome = new ResultGUI(solution1);
+                	outcome.frame.setVisible(true);
+			    }			
+			}
+		});
+		btnNewButton.setBounds(250, 184, 139, 29);
+		frame.getContentPane().add(btnNewButton);
+		
+		JLabel lblNewLabel = new JLabel("Please choose a method: ");
+		lblNewLabel.setFont(new Font("Lucida Grande", Font.BOLD | Font.ITALIC, 21));
+		lblNewLabel.setBounds(57, 63, 387, 38);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new BevelBorder(BevelBorder.LOWERED, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK));
+		panel_1.setBounds(23, 19, 400, 222);
+		frame.getContentPane().add(panel_1);
+	}
+}
+
+
